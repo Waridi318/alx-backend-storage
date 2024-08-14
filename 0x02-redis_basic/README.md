6210efa87418fff95e53598c51ac00b326bf84d9
Waridi318 <rosenjeri7151@gmail.com>
2024-08-14 22:27:31 +0300

    Task 1

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,3 @@
+## Learning Objectives:<br>
+- Learn how to use redis for basic operations
+- Learn how to use redis as a simple cache
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""This module is a practice exercise on using
+Redis
+"""
+
+
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """This class creates an instnce of the Redis class
+    """
+
+    def __inti__(self):
+        """Constructor"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in Redis with a randomly generated key.
+
+        Args:
+        data (Union[str, bytes, int, float]): The data to store.
+
+        Returns:
+        str: The key associated with the stored data.
+        """
+
+        key = str(uuid.uuid4())
+        self.set = (key, data)
+        return key
