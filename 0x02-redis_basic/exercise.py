2f00343b4995818a0f9ec0ddb6eeb089047c1f4b
Waridi318 <rosenjeri7151@gmail.com>
2024-08-14 23:30:46 +0300

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Redis
 
 
 import redis
-from typing import Union
+from typing import Union, Any, Optional, Callable
 import uuid
 
 
@@ -32,3 +32,39 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable[[Any], Any]] = None) -> Any:
+        """
+        Takes data as input and returns a modified version
+
+        Args:
+        key: str: The key associated with the data
+        fn: The function that converts the data
+        to the desired format
+
+        Returns:
+        Any: The data in the modified version
+        """
+
+        if key:
+            data = self._redis.get(key)
+            if data is not None and fn:
+                data = fn(data)
+            return data
+        return None
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Converts the data back to a string
+        """
+
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, data: Any) -> int:
+        """
+        Converts the data back to an integer
+        """
+
+        return self.get(key, fn=int)
